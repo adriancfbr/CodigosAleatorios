5a0e2b9b381e8d40b789b2f525222189a398a007
αdяiαиcf <adriancf@outlook.com.br>
2016-09-01 18:52:51 -0300

    Create ProjetoVideo.java

---
+++ ProjetoVideo.java
@@ -0,0 +1,27 @@
+ Index :
+  - (Cada item falado)
+  * (Topicos dos itens)
+  $ (Curiosidade)
+  # (Propaganda G2A)
+ 
+////////////////////////////////////////////////////////////////////////////////////////
+
+ - Inicia falando que o alcool esta na sociedade a muito tempo (2.600 a 2.350 A.C);
+ #
+ - Marketing: 
+             * Publico alvo 
+             * Questão cultural da cerveja e relação entre o clima e tipo de bebida (Cerveja trincando).
+             $ Consumo de cerveja total no mundo e consumo per capita (e a relevancia do brasil neste mercado)
+ - O por que dos jovens usar álcool
+ - O que o alcool pode causar: 
+                              * No humor           
+                              * No corpo
+                              * No psicológicos 
+                              $ Entre outros
+
+Cenas de fundo:
+ - Hancock
+ - Se beber nao case
+ - Edição Dorgas - TEAM G2A
+ - Cenas de boate
+
